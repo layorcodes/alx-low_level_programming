8ce93f11e38e1a720caa06f1d31b79c9857cc35f
layorcodes <tiphilayo@gmail.com>
2022-04-12 14:19:23 -0700

    103

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -14,7 +14,7 @@ int main(void)
 	k = 2;
 	sum = 0;
 
-	for (i =1; i <= 33; ++i)
+	for (i = 1; i <= 33; ++i)
 	{
 		if (j < 4000000 && (j % 2) == 0)
 		{
@@ -24,7 +24,7 @@ int main(void)
 		j = k;
 		k = next;
 	}
-	
+
 	printf("%lu\n", sum);
 
 	return (0);
