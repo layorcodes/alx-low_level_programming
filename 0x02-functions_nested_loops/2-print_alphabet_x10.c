a42aa47e255e686244edd980b2e6d0d7d2253e62
layorcodes <tiphilayo@gmail.com>
2022-04-12 11:01:10 -0700

    TWo

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -17,12 +17,12 @@ void print_alphabet_x10(void)
 		while (letter <= 'z')
 		{
 			_putchar(letter);
-			letter++
+			letter++;
 		}
 
-		_putchar('\n')
+		_putchar('\n');
 
-	        round++
+	        round++;
 	}
 }
 
