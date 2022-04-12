a0a37af0a674a5fa62b70e89e1b4c4c541fc9394
layorcodes <tiphilayo@gmail.com>
2022-04-12 07:57:57 -0700

    task3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,10 +2,10 @@
 
 /**
  * _islower - Checks if a character is lowercase
- * c is the character to be checked
- * Return 1 if the character is lowercase, 0 otherwise
+ * @c: the character to be checked
+ *
+ * Return: 1 if the character is lowercase, 0 otherwise
  */
-
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
