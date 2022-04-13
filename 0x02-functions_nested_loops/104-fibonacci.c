9a53df3a314e1048708f9c5bd9c03531d3693ae0
layorcodes <tiphilayo@gmail.com>
2022-04-13 07:59:47 -0700

    This does not seem to be working

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - main block
- * Description: computes and prints the sum of all the multiples of 3 or
- * 5 below 1024 (excluded), followed by a new line
- * Return : Always 0 (success)
+ * main - finds and prints the first 98 Fibonacci numbers,
+ * starting with 1 and 2
+ * followed by a new line
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
