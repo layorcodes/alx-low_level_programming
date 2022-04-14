6747037b592d79da7b036f1a4665ffd2dbe1141f
layorcodes <tiphilayo@gmail.com>
2022-04-14 14:08:46 -0700

    Fizzbuzz

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - a function that draws astraight line in the terminal
+ * followed by a new line
+ * @n: An input integer
+ * Return: always (0)
+ */
+void print_line(int n)
+{
+	int i = 0;
+
+	if (n > 0)
+	{
+		for (; i < n; i++)
+			_putchar('_');
+	}
+	_putchar('\n');
+}
