ea5ae879c8395f69cfc9d869b1bd202058e99850
layorcodes <tiphilayo@gmail.com>
2022-04-25 12:29:19 -0700

    reverse stringss

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -13,7 +13,7 @@ void reverse_array(int *a, int n)
 
 	for (m = 0; m < n - 1; m++)
 	{
-		for (j = i + 1; j > 0; j--)
+		for (j = m + 1; j > 0; j--)
 		{
 			rev = *(a + j);
 			*(a + j) = *(a + (j - 1));
