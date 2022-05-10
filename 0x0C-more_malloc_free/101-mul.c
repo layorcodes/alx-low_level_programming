482374dcdc5e9e5803a66eacb8d53ee70838254a
layorcodes <tiphilayo@gmail.com>
2022-05-10 11:27:43 -0700

    mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -49,16 +49,16 @@ void errors(void)
 }
 
 /**
- * main - multiplies two postive nums.
+ * main - multiplies two postive numbers.
  * @argc: number of arguments.
  * @argv: array of arguments.
  *
- * Return: always ) (Success)
+ * Return: always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
 	char *s1, *s2;
-	int len1, len2, len i, carry, digit1, digit2, *result, a = 0;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
 	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
@@ -83,7 +83,7 @@ int main(int argc, char *argv[])
 			carry /= 10;
 		}
 		if (carry > 0)
-			result[len1 +len2 + 1] += carry;
+			result[len1 + len2 + 1] += carry;
 	}
 	for (i = 0; i < len - 1; i++)
 	{
