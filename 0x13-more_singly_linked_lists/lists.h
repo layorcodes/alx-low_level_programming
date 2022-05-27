41fa7daaf2d621234cc5b69ab3ebe7b5f5cf2060
layorcodes <tiphilayo@gmail.com>
2022-05-27 00:51:16 -0700

    laides

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,17 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct listint_s - singly linked list.
+ * @n: integer.
+ * @next: pointer to the next node.
+ *
+ * Description: singly linked list struct node
+ * for this project.
+ */
+
+typedef struct listint_s
+{
